c95c4d4a2f28d35152a81cdd0e38f825039f7e12
zarlo265 <steven.timothy.cp@gmail.com>
2017-10-14 19:05:06 -0600

    hello with authentication

--- Querier/Querier/Controllers/HomeController.cs
+++ Querier/Querier/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Querier.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Querier.Controllers
 {
@@ -15,6 +16,7 @@ namespace Querier.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -22,6 +24,7 @@ namespace Querier.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
