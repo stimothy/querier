97c86e094f7470451b86080e14ffbc57446bd14e
dGohier <daniel.gohier@usu.edu>
2017-10-17 21:04:29 -0600

    Development Mode
    Switched the Configure method to use the development environment.

--- Querier/Querier/Startup.cs
+++ Querier/Querier/Startup.cs
@@ -47,16 +47,16 @@ namespace Querier
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
                 app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
 
             app.UseStaticFiles();
 
