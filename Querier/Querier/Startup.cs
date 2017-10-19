f71715d76b4b50bd70ad5ecd594c07b00122b594
zarlo265 <steven.timothy.cp@gmail.com>
2017-10-19 15:15:17 -0600

    connected new database and made tables.

--- Querier/Querier/Startup.cs
+++ Querier/Querier/Startup.cs
@@ -26,14 +26,10 @@ namespace Querier
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Use this when you want a local db.
-            /*services.AddDbContext<ApplicationDbContext>(options =>
+            //Tells the application which database to connect to.
+            services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            */
-
-            services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationDbContext>(options => 
-            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-
+            
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
