69b16438813f9ff2df23b1e4b161ed4736285015
Spencer Thorson <spencermt2012@gmail.com>
2017-10-25 10:53:42 -0600

    created manage query2 nav link, with model and create button

--- Querier/Querier/Models/ManageQueryModel.cs
+++ Querier/Querier/Models/ManageQueryModel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Querier.Models
 {
+    
     public class ManageQueryModel
     {
+        //private int PrivUserID { get; set; }
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public int UserID
+        //{
+        //    get { return PrivUserID; }
+        //}
+        //private int PrivQueryID { get; set; }
+        //[ForeignKey("ManageQuestionModel")]
+        //public int QueryID
+        //{
+        //    get { return PrivQueryID; }
+        //}
+        [Key]
+        public int UserID { get; set; }
+        [ForeignKey("ManageQuestionModel")]
+        [Editable(false)]
+        public int QueryID { get; set; }
+        [Display (Name="Query Name")]
+        public string QueryName { get; set; }
     }
 }
