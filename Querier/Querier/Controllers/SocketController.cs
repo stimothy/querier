8416207ecb6b61d48d148671b379b4e16aa727d6
zarlo265 <steven.timothy.cp@gmail.com>
2017-11-28 13:10:48 -0700

    changed database

--- Querier/Querier/Controllers/SocketController.cs
+++ Querier/Querier/Controllers/SocketController.cs
@@ -25,20 +25,24 @@ namespace Querier.Controllers
         [NonAction]
         public async void Server(HttpContext context)
         {
-                if (context.WebSockets.IsWebSocketRequest)
+            //var wssv = new WebSocketServer("ws://localhost:");
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                byte[] buffer = new byte[4 * 1024];
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                string msg = Encoding.BigEndianUnicode.GetString(buffer, 0, buffer.Length);
+                if (webSocket != null && webSocket.State == WebSocketState.Open)
                 {
-                    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                    if (webSocket != null && webSocket.State == WebSocketState.Open)
+                    while (webSocket.State != WebSocketState.Closed)
                     {
-                        while (webSocket.State != WebSocketState.Closed)
-                        {
-                            string msg = "Hi from Server controller.";
-                            byte[] sendBuffer = Encoding.Unicode.GetBytes(msg);
-                            await webSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
-                            System.Threading.Thread.Sleep(1000);
-                        }
+                        //string msg = "Hi from Server controller.";
+                        byte[] sendBuffer = Encoding.Unicode.GetBytes(msg);
+                        await webSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                        System.Threading.Thread.Sleep(1000);
                     }
                 }
+            }
         }
 
         //[HttpGet("v1/resources/{id}")]
